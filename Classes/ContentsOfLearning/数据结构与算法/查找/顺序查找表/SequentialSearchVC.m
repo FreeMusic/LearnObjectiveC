0f9c00a396eb5ee22eaf99aa8b17177f0f8268e2
孙晓阳 <sunxiaoyang@sunxiaongdeiMac.lan>
2019-08-17 22:35:33 +0800

    2019年08月17日22:35:30

--- Classes/ContentsOfLearning/数据结构与算法/查找/顺序查找表/SequentialSearchVC.m
+++ Classes/ContentsOfLearning/数据结构与算法/查找/顺序查找表/SequentialSearchVC.m
@@ -11,6 +11,10 @@
  */
 #import "SequentialSearchVC.h"
 
+#define MaxSize 100 //存储空间初始分配量
+
+int Fib[MaxSize];//斐波那契数列
+
 @interface SequentialSearchVC ()
 
 @end
@@ -21,8 +25,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    int list[MaxSize+1], i;
+    int array[MaxSize] = {0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99};
     
+    for (i = 0; i <= MaxSize; i++) {
+        list[i] = i;
+    }
+    //无哨兵顺序查找
+    WithOutSentrySequentialSearch(list, MaxSize, MaxSize*2);
+    //有哨兵顺序查找
+    SentrySequentialSearch(list, MaxSize, MaxSize/2);
 }
 
+/**
+ 无哨兵顺序查找
+
+ @param list 数组
+ @param n 要查找的数组的个数
+ @param key 要查找的关键字
+ */
+void WithOutSentrySequentialSearch(int *list, int n, int key)
+{
+    int i;
+    
+    for (i = 1; i <= n; i++) {
+        if (list[i] == key) {
+            RYQLog(@"无哨兵顺序查找 ********** 搜查到查找的对象 key = %d", list[i]);
+            return;
+        }
+    }
+    RYQLog(@"无哨兵顺序查找 ********** 没有搜查到查找的对象");
+}
+
+/**
+ 有哨兵顺序查找
+
+ @param list 数组
+ @param n 要查找的数组的个数
+ @param key 要查找的关键字
+ */
+void SentrySequentialSearch(int *list, int n, int key)
+{
+    int i;
+    list[0] = key;
+    i = n;
+    while (list[i] != key) {
+        i--;
+    }
+    if (i== 0) {
+        RYQLog(@"有哨兵顺序查找 ********** 没有搜查到查找的对象");
+    }else{
+        RYQLog(@"有哨兵顺序查找 ********** 搜查到查找的对象 key = %d", list[i]);
+    }
+}
 
 @end
